07424f8f3d15ffd309c16c115520dae1be047119
nvakiet <nvakiet191@gmail.com>
2022-04-16 19:28:54 +0700

    changed server back to NioSocket server

--- Server/src/com/fz/foodzoneserver/server/Main.java
+++ Server/src/com/fz/foodzoneserver/server/Main.java
@@ -8,14 +8,14 @@ public class Main {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
-            //System.out.print("Input database connection: ");
-            //String dbConnString = sc.nextLine();
+            System.out.print("Input database connection: ");
+            String dbConnString = sc.nextLine();
             System.out.print("Input bind IP: ");
             String ip = sc.nextLine();
-            //String dbConnString = "jdbc:sqlserver://DESKTOP-18BTLCH\\SQLEXPRESS;databaseName=PrayForFood;integratedSecurity=true;";
-            String dbConnString = "jdbc:sqlserver://localhost\\SQLSERVER;databaseName=PrayForFood;integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
+            //String dbConnString = "jdbc:sqlserver://DESKTOP-18BTLCH\\SQLEXPRESS;databaseName=FoodZoneDB;integratedSecurity=true;";
+            //String dbConnString = "jdbc:sqlserver://localhost\\SQLSERVER;databaseName=FoodZoneDB;integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
 
-            ServerFoodZone server = new ServerFoodZone(dbConnString, ip, 8899);
+            ServerFoodZone server = new ServerFoodZone(dbConnString, ip, 9999);
             server.start();
             int code = sc.nextInt();
             if (code == 0)
