4d6e26ab42389e1c962c213fdb993f11bfb7ed8d
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2022-05-03 23:52:03 +0700

    init success screen

--- Client/app/src/main/java/com/example/foodzoneclient/ui/SuccessScreenActivity.java
+++ Client/app/src/main/java/com/example/foodzoneclient/ui/SuccessScreenActivity.java
@@ -1,25 +1,26 @@
 package com.example.foodzoneclient.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.foodzoneclient.R;
 
 public class SuccessScreenActivity extends AppCompatActivity {
-    Button returnMain;
+    Button   returnMain;
     TextView textDescription;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success_screen);
-        returnMain=(Button) findViewById(R.id.returnMain);
-        textDescription=(TextView) findViewById(R.id.textDes);
-        Intent thisIntent= this.getIntent();
-        String temp=thisIntent.getStringExtra("successString");
+        returnMain      = (Button) findViewById(R.id.homeBtn);
+        textDescription = (TextView) findViewById(R.id.textDes);
+        Intent thisIntent = this.getIntent();
+        String temp       = thisIntent.getStringExtra("successString");
         textDescription.setText(temp);
 
         returnMain.setOnClickListener(new View.OnClickListener() {
